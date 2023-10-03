e9224d6d980495aa4a290ee4900166f4a1eb796d
Karimov Bulat <karimov@blue-ant.ru>
2023-10-03 15:54:02 +0300

    chore: run gulp or nuxt projects

--- app.Dockerfile
+++ app.Dockerfile
@@ -4,5 +4,7 @@ COPY certs/mitmproxy-ca.pem /usr/local/share/ca-certificates/mitmproxy-ca.pem
 RUN cat /usr/local/share/ca-certificates/mitmproxy-ca.pem >> /etc/ssl/certs/ca-certificates.crt
 RUN rm /usr/local/share/ca-certificates/mitmproxy-ca.pem 
 RUN apk update && apk upgrade && apk add nodejs npm
+COPY entrypoint.sh /tmp/entrypoint.sh
+RUN chmod +x /tmp/entrypoint.sh
 USER 1000:1000
 ENV NODE_OPTIONS=--openssl-legacy-provider
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+if [ ! -d node_modules ];then
+    echo 'Not a project' 
+    exit 1
+fi
+
+
+run_gulp() {
+    node --inspect=0.0.0.0:9229 ./node_modules/.bin/gulp dev
+}
+
+run_nuxt() {
+    if npx nuxt dev -h | grep -wq 'hostname'; then
+        HOSTNAME_CMD='hostname'
+    else
+        HOSTNAME_CMD='host'
+    fi
+
+    node --inspect=0.0.0.0:9229 ./node_modules/.bin/nuxt dev --${HOSTNAME_CMD} 0.0.0.0 --port 3000
+}
+
+
+if [ -f gulpfile.js ]; then
+    run_gulp
+else
+    run_nuxt
+fi
